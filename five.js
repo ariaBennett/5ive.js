d32af849e0ad85cf359bcaa023899fd8513768c2
Aria Bennett Troutt <twoplustwo@gmail.com>
2014-03-07 19:25:02 -0800

    Optimized conditional fall-through sequence.
    Run-time was previously optimized for improper use-cases which, while theoretically possible, have never been reported.  
    
    The new enhanced algorithm optimizes proper use cases, reducing the lines processed by most invocations by 20-30%.

--- five.js
+++ five.js
@@ -2,10 +2,10 @@ Fivejs = {};
 
 Fivejs.five = function(num) {
   try {
-    if (typeof num !== 'number') {
-      throw "invalid input to FiveJs.five() " + num + ' is not a number.';
-    } else {
+    if (typeof num === 'number') {
       return (num*5/5 ) + 5 - 5;
+    } else {
+      throw "invalid input to FiveJs.five() " + num + ' is not a number.';
     }
   } catch (err) {
     console.log('Error: ' + err);
